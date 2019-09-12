fd188e07615f995b233aef356fc59e162619dc98
Lucia Reynoso <lvreynoso@protonmail.com>
2019-09-11 22:07:08 -0700

    Set up webpack, react, node, express, and babel

---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Index = () => {
+  return <div>Welcome to React!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
