206a51477932243f8f6f585c80d2784d04bd9055
Ali Shalabi <ashalabi13@gmail.com>
2019-09-29 10:24:13 -0700

    Added baseline code, ES5 notation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@
 // dependencies
 import express from 'express'
 import path from 'path'
+// Old version:
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const db = require('./db'); // placeholder
+// New version
+// import passport from 'passport'
+// import strategy from 'passport-local'
 
 // set our express options
 const app = express();
@@ -11,10 +18,31 @@ const REACT_DIR = path.join(__dirname, '../client'); // NEW
 const HTML_FILE = path.join(REACT_DIR, 'index.html'); // NEW
 app.use(express.static(REACT_DIR)); // NEW
 
+// Local Authentication
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+
 // placeholder route
 app.get('/', (req, res) => {
  res.sendFile(HTML_FILE); // EDIT
 });
+
+// placeholder: login
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
 
 // face the world
 const hotPort = app.get('port');
