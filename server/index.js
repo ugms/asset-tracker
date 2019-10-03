7395ac196e52ec0825316449d2d1ce4b0d6595b6
Lucia Reynoso <lvreynoso@protonmail.com>
2019-10-03 10:06:46 -0700

    Add linter and precommit hooks (#5)
    * Add eslint and prettier
    
    * Update package.json
    
    * Remove precommit script
    
    * Fix linter errors
    
    * Add babel parser for eslint and lint script
    
    * Update TravisCI script
    
    * Update TravisCI script to add PostgreSQL
    
    * Update TravisCI command to createdb

--- server/index.js
+++ server/index.js
@@ -1,49 +1,52 @@
-'use strict'
-
 // environment setup
-import dotenv from 'dotenv'
-const result = (process.env.NODE_ENV == 'development') ? dotenv.config() : false
+import dotenv from "dotenv";
 
 // dependencies
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
 
 // SQL / db
-import Sequelize from 'sequelize';
+import Sequelize from "sequelize";
+
+const result = process.env.NODE_ENV === "development" ? dotenv.config() : false;
+if (result) {
+  console.log("Node.js running in development mode");
+}
+
 const sequelize = new Sequelize(
   process.env.DATABASE,
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
-    dialect: 'postgres',
-  },
+    dialect: "postgres"
+  }
 );
 const models = {
-  User: sequelize.import('./models/user'),
+  User: sequelize.import("./models/user")
 };
 Object.keys(models).forEach(key => {
-  if ('associate' in models[key]) {
+  if ("associate" in models[key]) {
     models[key].associate(models);
   }
 });
 
 // set our express options
 const app = express();
-app.set('port', process.env.PORT || 3000);
-const REACT_DIR = path.join(__dirname, '../client'); // NEW
-const HTML_FILE = path.join(REACT_DIR, 'index.html'); // NEW
-app.use(express.static(REACT_DIR)); // NEW
+app.set("port", process.env.PORT || 3000);
+const REACT_DIR = path.join(__dirname, "../client");
+const HTML_FILE = path.join(REACT_DIR, "index.html");
+app.use(express.static(REACT_DIR));
 
 // placeholder route
-app.get('/', (req, res) => {
- res.sendFile(HTML_FILE); // EDIT
+app.get("/", (req, res) => {
+  res.sendFile(HTML_FILE);
 });
 
 // face the world
-const hotPort = app.get('port');
+const hotPort = app.get("port");
 const server = app.listen(hotPort, () => {
-    console.log(`Example app listening on port ${hotPort}!`)
-    sequelize.sync();
+  console.log(`Asset-tracker listening on port ${hotPort}!`);
+  sequelize.sync();
 });
 
 // for Mocha/Chai test purposes
