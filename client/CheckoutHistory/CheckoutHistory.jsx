78d4ae784ae2f239906fcd3e7124b5b1aba0fe24
Anna Pawl <annampawl@gmail.com>
2019-10-08 10:49:29 -0700

    checkout history page

---
+++ client/CheckoutHistory/CheckoutHistory.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Navbar from '../Navbar/Navbar'
+
+class CheckoutHistory extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+      <div>
+
+
+
+        <h1>This is the Checkout History Page</h1>
+      </div>
+    )
+  }
+}
+
+
+
+export default CheckoutHistory
