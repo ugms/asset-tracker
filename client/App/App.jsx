4a26ad491ee7107d9d3426b46a981952f1ccd0a8
Anna Pawl <annampawl@gmail.com>
2019-11-05 06:37:04 -0800

    new item styles

--- client/App/App.jsx
+++ client/App/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-// import logo from "../static/logo.svg";
-import Login from '../Login/Login.jsx';
-import Dashboard from '../Dashboard/Dashboard.js';
-import InventoryOverview from '../InventoryOverview/InventoryOverview.jsx'
-import CheckoutForm from '../CheckoutForm/CheckoutForm.jsx'
 import NewItemForm from '../NewItemForm/NewItemForm.jsx'
-import InventoryOverviewTest from '../InventoryOverview/InventoryOverviewTest.jsx'
+
 
 import "./App.css";
 
@@ -15,12 +10,9 @@ function App() {
     <div className="App">
       <Router>
 
-        <Route exact path="/Login" exact component={Login} />
-        <Route exact path="/Dashboard" exact component={Dashboard} />
-        <Route exact path="/Inventory-Overview" exact component={InventoryOverview} />
-        <Route exact path="/Checkout-Form" exact component={CheckoutForm} />
+
         <Route exact path="/New-Item-Form" exact component={NewItemForm} />
-        <Route exact path="/Inventory-Overview-Test" exact component={InventoryOverviewTest} />
+
 
       </Router>
     </div >
--- client/NewItemForm/NewItemForm.jsx
+++ client/NewItemForm/NewItemForm.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './NewItemForm.scss'
-import Navbar from '../Navbar/Navbar'
 
 class NewItemForm extends Component {
   constructor(props) {
@@ -10,9 +9,9 @@ class NewItemForm extends Component {
       id: '',
       email: '',
       phoneNumber: '',
-      checkoutDuration: '',
+      new_itemDuration: '',
       date: '',
-      reasonForCheckout: '',
+      reasonFornew_item: '',
 
     }
   }
@@ -20,22 +19,22 @@ class NewItemForm extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <div className='checkout_form_container'>
-          <div className='checkout_square'>
-            <h1>New Item</h1>
-            <form className='checkout_form'>
+
+        <div className='new_item_form_container'>
+          <div className='new_item_square'>
+            <h1 className='new_item_title'>New Item</h1>
+            <form className='new_item_form'>
 
               {/* Chassie */}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Chassie' />
 
               {/* Current User */}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Current User'
@@ -43,42 +42,42 @@ class NewItemForm extends Component {
 
               {/* Name2 */}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Name2' />
 
               {/* Manufacturer */}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Manufacturer' />
 
               {/* Model */}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Model' />
 
               {/* Operating System */}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='type'
                 value=''
                 placeholder='Operating System' />
 
               {/* Serial Number*/}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Serial Number' />
 
               {/* Purchase Date*/}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Purcahse Date' />
@@ -86,14 +85,14 @@ class NewItemForm extends Component {
 
               {/* Notes*/}
               <input
-                className='input_field'
+                className='new_item_input_field'
                 type='text'
                 value=''
                 placeholder='Notes' />
 
             </form>
 
-            <button>Checkout</button>
+            <button className='new_item_form_button'>Create<br /> New Item</button>
           </div>
         </div>
       </div>
