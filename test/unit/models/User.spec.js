7dcc2c3f1791c27e381b01a7c6eb1cbdc166e748
Ali Shalabi <ashalabi13@gmail.com>
2019-11-06 11:59:45 -0800

    Added structure, working on first user test

---
+++ test/unit/models/User.spec.js
@@ -0,0 +1,41 @@
+/*
+Built-In Tests
+
+checkHookDefined: Checks that a particular hook is defined.
+checkModelName: Checks that the model is named correctly.
+checkNonUniqueIndex: Checks that a specific non-unique index is defined.
+checkPropertyExists: Checks that the model has defined the given property.
+checkUniqueCompoundIndex: Checks that a specific unique compound index is defined.
+checkUniqueIndex: Checks that a specific unique index is defined.
+
+*/
+
+
+const { expect } = require('chai')
+
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists
+} = require('sequelize-test-helpers')
+
+const UserModel = require('../../../server/models/user')
+
+describe('server/models/User', () => {
+  const User = UserModel(sequelize, dataTypes)
+  const user = new User()
+
+  checkModelName(User)('User')
+
+  context('properties', () => {
+    ;['username'].forEach(
+      checkPropertyExists(user)
+    )
+  })
+
+  // context('indexes', () => {
+  //   ;['email', 'token'].forEach(checkUniqueIndex(user))
+  // })
+})
