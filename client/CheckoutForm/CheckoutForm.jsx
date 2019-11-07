fcd4e7c3525de2517d22c91263af8861c1b67220
Anna Pawl <annampawl@gmail.com>
2019-11-06 23:54:22 -0800

    linting checkout form

--- client/CheckoutForm/CheckoutForm.jsx
+++ client/CheckoutForm/CheckoutForm.jsx
@@ -12,8 +12,31 @@ class CheckoutForm extends Component {
       checkoutDuration: '',
       date: '',
       reasonForCheckout: '',
-
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  // handleChange(event) {
+  //   this.setState({value: event.target.value});
+  // }
+
+  handleChange = (e) => {
+    const newState = {}
+    newState[e.target.name] = e.target.value,
+      newState[e.target.id] = e.target.value,
+      newState[e.target.email] = e.target.value,
+      newState[e.target.phoneNumber] = e.target.value,
+      newState[e.target.checkoutDuration] = e.target.value,
+      newState[e.target.date] = e.target.value,
+      newState[e.target.reasonForCheckout] = e.target.value,
+
+      this.setState(newState)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
   }
 
   render() {
@@ -22,20 +45,23 @@ class CheckoutForm extends Component {
         <div className='checkout_form_container'>
           <div className='checkout_square'>
             <h1 className='checkout_title'>Checkout Form</h1>
-            <form className='checkout_form'>
+            <form className='checkout_form' state={this.state} onChange={state => this.setState(state)}>
 
               {/* Name Field */}
               <input
                 className='checkout_input_field'
                 type='text'
-                value=''
-                placeholder='Name' />
+                value={this.name}
+                onChange={this.handleChange}
+                placeholder='Name'
+              />
 
               {/* Company ID Field */}
               <input
                 className='checkout_input_field'
                 type='text'
-                value=''
+                value={this.id}
+                onChange={this.handleChange}
                 placeholder='Company ID'
               />
 
@@ -43,36 +69,48 @@ class CheckoutForm extends Component {
               <input
                 className='checkout_input_field'
                 type='text'
-                value=''
-                placeholder='Email Address' />
+                value={this.email}
+                onChange={this.handleChange}
+                placeholder='Email Address'
+              />
 
               {/* Phone Number Field  */}
               <input
                 className='checkout_input_field'
                 type='text'
-                value=''
-                placeholder='Phone Number' />
+                value={this.phoneNumber}
+                onChange={this.handleChange}
+                placeholder='Phone Number'
+              />
 
               {/* Duration of Checkout Field */}
               <input
                 className='checkout_input_field'
                 type='text'
-                value=''
-                placeholder='Duration of Checkout' />
+                value={this.checkoutDuration}
+                onChange={this.handleChange}
+                placeholder='Duration of Checkout'
+              />
 
-              {/* Date Field */} {/* TODO - make dropdown calendar */}
+              {/* Date Field */}
+              {' '}
+              {/* TODO - make dropdown calendar */}
               <input
                 className='checkout_input_field'
                 type='type'
-                value=''
-                placeholder='Date' />
+                value={this.date}
+                onChange={this.handleChange}
+                placeholder='Date'
+              />
 
               {/* Reason for Checkout Field */}
               <input
                 className='checkout_input_field'
                 type='text'
-                value=''
-                placeholder='Reason for Checkout' />
+                value={this.reasonForCheckout}
+                onChange={this.handleChange}
+                placeholder='Reason for Checkout'
+              />
 
             </form>
 
@@ -83,6 +121,7 @@ class CheckoutForm extends Component {
     )
   }
 }
+
 
 
 export default CheckoutForm
