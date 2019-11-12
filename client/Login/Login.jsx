8efac80266bc700459a2e9e12a19df260b97e929
Anna Pawl <annampawl@gmail.com>
2019-11-12 08:05:08 -0800

    refactored login

--- client/Login/Login.jsx
+++ client/Login/Login.jsx
@@ -2,85 +2,119 @@ import React, { Component } from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 
-const Input = props => (
-  <input
-    className="c-input"
-    type={props.type}
-    placeholder={props.placeholder}
-    onKeyUp={props.onKeyUp}
-  />
-);
+// const Input = props => (
+//   <input
+//     className="c-input"
+//     type={props.type}
+//     placeholder={props.placeholder}
+//     onKeyUp={props.onKeyUp}
+//   />
+// );
 
-const Button = props => (
-  <button className="c-button" onClick={props.onClick}>
-    {props.text}
-  </button>
-);
+// const Button = props => (
+//   <button className="c-button" onClick={props.onClick}>
+//     {props.text}
+//   </button>
+// );
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: { value: "" },
-      password: { value: "" }
+      username: '',
+      password: '',
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleInput(param, e) {
-    let value = e.target.value;
+  componentDidMount() {
+    console.log('I was triggered during componentDidMount')
+  }
 
-    console.log(param);
+  handleSubmit(e) {
+    e.preventDefault();
+    // const data = { username: this.state.username, password: this.state.password };
+    const { username, password } = this.state
+    const data = { username, password }
+    console.log(data)
+    fetch('/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      const response = res.json()
+      console.log(response)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
-    if (param === "name") {
-    }
-  };
 
   render() {
-    console.log('Working')
+    const { username, password } = this.state
     return (
       <div>
 
         <div>
 
-          <div className="blackbg"></div>
+          <div className="blackbg" />
           <div className="orangebg">
-            <img src="https://i.ibb.co/0GR3kX3/upwardlyglobal.png"
+            <img
+              src="https://i.ibb.co/0GR3kX3/upwardlyglobal.png"
               className="logo"
-            /></div>
-          <section id="login">
+              alt='upwardly global logo'
+            />
+          </div>
+          <section id="register">
             <div className="container ">
 
               <div className="card">
 
                 <div className="card-title">
 
-                  <h2>LOGIN</h2>
+                  <h2>login</h2>
                 </div>
                 <div className="card-body">
-                  <form className="form">
-                    <Input
+                  <div className="form">
+
+                    <input
+                      className="c-input"
                       type="text"
                       placeholder="USERNAME"
-                      value={this.state.email.value}
-                      onKeyUp={e => this.handleInput("email", e)}
+                      value={username}
+                      onChange={e => this.setState({ username: e.target.value })}
                     />
-                    <Input
+
+                    <input
+                      className="c-input"
                       type="password"
                       placeholder="PASSWORD"
-                      value={this.state.password.value}
-                      onKeyUp={e => this.handleInput("password", e)}
+                      value={password}
+                      onChange={e => this.setState({ password: e.target.value })}
                     />
-                  </form>
-                  <div className="loginbutton">
-                    <Button type='submit ' text="LOGIN" onClick={this.handleSubmit} />
+
+                  </div>
+                  <div className="login_button">
+
+                    <button
+                      label="Button"
+                      type="button"
+                      className="c-button"
+                      text="Login"
+                      onClick={(e) => this.handleSubmit(e)}
+                    >
+                      Login
+                    </button>
+
                   </div>
 
-                  <div className='register_link_container'>
-                    <Link to="/Register" className="register_link">
-                      Don't have an account?
+                  <div className='login_link_container'>
+                    <Link to="/Register" className="registration_link">
+                      Don't Have an Account?
                       <br />
 
-                      <span className='register_now_span'>  Register Now</span>
+                      <span className='register_now_span'>  Register Here</span>
                     </Link>
                   </div>
 
