807414070389556fb8487dad6f90b8414818366c
Ali Shalabi <ashalabi13@gmail.com>
2019-11-25 09:06:37 -0800

    Added authentication controller, having import issues

---
+++ server/controllers/authentication.js
@@ -0,0 +1,37 @@
+export default function(app, db) {
+
+  // Helper Function (For Authentication)
+  const getUser = async obj => {
+    return await db.User.findOne({
+    where: obj,
+    });
+  };
+
+  // Test login route
+  app.post("/auth-login", async function(req, res, next) {
+    const { username, password } = req.body;
+    if (username && password) {
+      // we get the user with the name and save the resolved promise
+      returned
+      let user = await getUser({ username });
+      if (!user) {
+        res.status(401).json({ msg: "No such user found", user });
+      }
+     if (user.password === password) {
+        // from now on we’ll identify the user by the id and the id is
+        // the only personalized value that goes into our token
+        let payload = { id: user.id };
+        let token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({ msg: ‘ok’, token: token });
+      } else {
+        res.status(401).json({ msg: "Password is incorrect" });
+      }
+    }
+  });
+
+  // Sample Protected Route
+  app.get("/protected", passport.authenicate("jwt", {session: false}), function(req, res) {
+    res.json({msg: "Success, you are seeing this because you are authorized"});
+  });
+
+}
--- server/index.js
+++ server/index.js
@@ -12,10 +12,11 @@ import Sequelize from "sequelize";
 
 // const passport = require('passport');
 import passport from "passport";
-import passportJWT from "passportJWT";
+import passportJWT from "passport-jwt";
 
 import userController from "./controllers/users";
 import assetController from "./controllers/assets";
+import authenticationController from "./controllers/authentication";
 
 const result = process.env.NODE_ENV === "development" ? dotenv.config() : false;
 if (result) {
@@ -70,13 +71,6 @@ let jwtOptions = {};
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOptions.secretOrKey = "samplesecret"
 
-// Helper Function (For Authentication)
-const getUser = async obj => {
-  return await db.User.findOne({
-  where: obj,
-  });
-};
-
 // Create Strategy
 let strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
   console.log("payload received", jwt_payload);
@@ -91,37 +85,10 @@ passport.use(strategy);
 
 app.use(passport.initalize());
 
-// Test login route
-app.post("/auth-login", async function(req, res, next) {
-  const { username, password } = req.body;
-  if (username && password) {
-    // we get the user with the name and save the resolved promise
-    returned
-    let user = await getUser({ username });
-    if (!user) {
-      res.status(401).json({ msg: "No such user found", user });
-    }
-   if (user.password === password) {
-      // from now on we’ll identify the user by the id and the id is
-      // the only personalized value that goes into our token
-      let payload = { id: user.id };
-      let token = jwt.sign(payload, jwtOptions.secretOrKey);
-      res.json({ msg: ‘ok’, token: token });
-    } else {
-      res.status(401).json({ msg: "Password is incorrect" });
-    }
-  }
-});
-
-// Sample Protected Route
-app.get("/protected", passport.authenicate("jwt", {session: false}), function(req, res) {
-  res.json({msg: "Success, you are seeing this because you are authorized"});
-});
-
-
 
 userController(app, models);
 assetController(app, models);
+authenticationController(app, models);
 
 
 // face the world
