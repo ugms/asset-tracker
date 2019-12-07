87bd417d0cd05dfbc1cbfc87d6f9a3a59052cec6
erica2605@live.com <erica2605@live.com>
2019-12-06 16:29:05 -0800

    attempt 1

---
+++ client/InventoryOverview/AssetTable2.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./AssetTable2.css";
+
+const AssetTable2 = ({ assets }) => {
+  return (
+    <div>
+      <table className="flat-table">
+        <tbody>
+          <tr>
+            <th>Chassie</th>
+            <th>Current User</th>
+            <th>Previous User</th>
+            <th>Previous User</th>
+            <th>Name 2</th>
+            <th>Manufacturer</th>
+            <th>Model</th>
+            <th>OS</th>
+            <th>Serial Number</th>
+            <th>Purchase Date</th>
+            <th>Notes</th>
+          </tr>
+          <tr>
+            <td>Example 1</td>
+            <td>Example 2</td>
+            <td>Example 3</td>
+            <td>Example 4</td>
+            <td>Example 5</td>
+            <td>Example 6</td>
+            <td>Example 7</td>
+            <td>Example 8</td>
+            <td>Example 9</td>
+            <td>Example 10</td>
+            <td>Example 11</td>
+          </tr>
+          <tr>
+            <td>Example 1</td>
+            <td>Example 2</td>
+            <td>Example 3</td>
+            <td>Example 4</td>
+            <td>Example 5</td>
+            <td>Example 6</td>
+            <td>Example 7</td>
+            <td>Example 8</td>
+            <td>Example 9</td>
+            <td>Example 10</td>
+            <td>Example 11</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AssetTable2;
